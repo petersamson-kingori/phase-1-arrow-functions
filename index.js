4a433bf7fe4d7aa13ea14d88307eb4a7691a26c1
petersamson-kingori <peters.kingori@student.moringaschool.com>
2022-12-07 10:04:46 +0300

    Completed lab

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+  const square = (x) =>{
+    return x * x;
+  };
+  square(5);
+
+  const add = (a,b) => {
+    return a + b
+};
+  add (2,2);
+  
+  const divide = () => {
+
+    return 2000/100;
+  }
+divide();
